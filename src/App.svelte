499076e97c8d83f3c49c538d653af25aa278c3f9
Nelson Bonilla <nelson.bonilla@globallogic.com>
2021-11-29 11:39:52 -0300

    primer push

---
+++ src/App.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import Child from './Child.svelte';
+	let name: string = 'Nelson';
+	let inputs = creatObj(8);
+	let _refs = []
+	let value = 0;
+	$: if (inputs.filter(m => m.selected).length >= 0) {
+		value = reduceFunction();
+	}
+	function creatObj(count) {
+		const p = [];
+		for(let i = 0; i < count; i++) {
+			p.push(
+				{
+					item: i,
+					key: i,
+					value: 0,
+					selected: false
+				}
+			);
+		}
+		return p;
+	}
+	const reduceFunction = () => {
+		const list = inputs.filter(m => m.selected).map(m => m.value);
+		if (list.length) {
+			return list.reduce((previousValue, currentValue) => previousValue + currentValue);
+		} 
+		return 0;
+	}
+	const resetList = () => {
+		for(let item of inputs) {
+			item.selected = false;
+		}
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<div style="display: flex; width: 100%">
+		{#each inputs as input, i}
+			<Child reset={resetList} bind:this={_refs[i]} bind:value={input.value} bind:selected={input.selected}/>
+		{/each}
+
+	</div>
+	<div>
+		{value}
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Child.svelte
@@ -0,0 +1,60 @@
+<svelte:options accessors={true}/>
+<script lang="ts">
+	export let value;
+	export let reset;
+	export let selected = false;
+	let ref;
+	let isCtrlKey;
+
+	const onInput = e => {
+		console.log(e)
+		if(e.relatedTarget?.classList.contains('ctrl-active')) {
+			selected = true;
+			isCtrlKey = false;
+		} else {
+			selected = false;
+			isCtrlKey = false;
+			reset();
+		}
+		value = parseFloat(e.target.value)
+	}
+	const onFocus = ev => {
+		selected = true;
+		if(isCtrlKey) {
+			
+		}
+	}
+	const ctrlKey = ev => {
+		if (ev.ctrlKey) {
+			isCtrlKey = true;
+		} else {
+			isCtrlKey = false;
+		}
+	}
+</script>
+
+<input class:selected class:ctrl-active={isCtrlKey} class="item" type="number" value={value} on:change={onInput} bind:this={ref} on:blur={onInput} on:focus={onFocus} on:mousedown={ctrlKey} on:mouseup={ctrlKey}>
+
+
+<style>
+	.item {
+		box-sizing: border-box;
+		width: 100%;
+		border: 1px solid;
+	}
+	.item:focus-visible {
+		/* border: none; */
+		outline:  none;
+	}
+	.item.selected {
+		border: 1px solid red;
+		background-color: rgba(255, 0, 0, .1)
+		
+	}
+
+	.item.ctrl-active {
+		border: 3px solid red;
+		background-color: rgba(255, 0, 0, .1)
+	}
+
+</style>
